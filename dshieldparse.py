d36c5d04539d8175603ea3b09db2edc3ca1d43e8
chrislss <tech@islandz.net>
2015-10-17 17:48:29 -0700

    Add python script and input file

---
+++ dshieldparse.py
@@ -0,0 +1,31 @@
+__author__ = 'chsmith4'
+#import requests
+
+#open the input file from SANS, the output file with modified IP's
+
+#inputfile = requests.get("http://feeds.dshield.org/block.txt")
+
+import urllib
+
+testfile = urllib.URLopener()
+testfile.retrieve("http://feeds.dshield.org/block.txt", "dshiledblock.txt")
+
+inputfile = open('dshieldblock.txt')
+outputfile = open('feedlist.txt','w')
+
+#Loop through the input file
+for line in inputfile:
+    #check for valid lines in the file
+    if line[0] <> '#' and line[0] <> " " and line[0] <> 'S':
+        #if a valid line parse the fields into a LIST with spaces as a delimiter (default)
+        linelist = line.split()
+        #verify the list is populated, if TRUE extract network range and write to new file
+        if linelist:
+            startip = linelist[0]
+            subnet = linelist[2]
+            network = startip + '/' + subnet + '\n'
+            outputfile.write(network)
+
+#cleanup - close open files
+inputfile.close()
+outputfile.close()
